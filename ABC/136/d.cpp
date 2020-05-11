011e04211a4dfdc00a36525e964a100cadce61b3
HikaruKondo <mushin.hudoushin@gmail.com>
2020-05-11 15:50:37 +0900

    ABC136

--- ABC/136/d.cpp
+++ ABC/136/d.cpp
@@ -20,6 +20,49 @@ typedef pair<int,int> P;
 #define S second
 
 int main(int argc, char const *argv[]) {
+    string s; cin >> s;
+    ll r[SIZE(s)] = {0}, l[SIZE(s)]={0}, ans[SIZE(s)]={0}, memo[SIZE(s)]={0};
+    ll temp = 0;
     
-    return 0;
+    // r[0] = 1;
+    REP(i,SIZE(s)) {
+        if (s[i] == 'R') temp++;
+        else {
+            r[i-1] = temp;
+            temp = 0;
+        }
+    }
+
+    // REP(i,SIZE(s)) cout << r[i] << " ";
+    // cout << "\n";
+    
+    temp = 0;
+    // l[SIZE(s)-1] = 1;
+    REPD(i,SIZE(s)) {
+        if (s[i] == 'L') temp++;
+        else {
+            l[i+1] = temp;
+            temp = 0;
+        }
+    }
+
+    // REP(i,SIZE(s)) cout << l[i] << " ";
+    // cout << "\n";
+
+    REP(i,SIZE(s)-1) {
+        if (s[i] == 'R' && s[i+1] == 'L') {
+            ll sum = r[i] + l[i+1];
+            if (sum % 2 == 0) {
+                ans[i] = sum/2;
+                ans[i+1] = sum/2;
+            }
+            else {
+                ans[i] = l[i+1]/2 + r[i]/2 + r[i]%2;
+                ans[i+1] = l[i+1]/2 + l[i+1]%2 + r[i]/2;
+            }
+        }
+    }
+
+    REP(i,SIZE(s)) cout << ans[i] << " ";
+    cout << "\n";
 }
